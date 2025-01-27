1130f903548bc4423e5a6ecfe0d9bb1a4ea12970
Muhammad Rashid <m.rashid212823@gmail.com>
2025-01-27 17:07:44 +0500

    Database Connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 
@@ -10,6 +11,16 @@ app.use(cors("*"));
 app.get("/", (req, res) => {
   res.send({ Message: "Hello World" });
 });
+console.log("mdb=>",process.env.MONGO_URI)
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("===========Database Connected===========");
+  })
+  .catch(() => {
+    console.log("===========Database Connection Failed===========");
+  });
 
 app.listen(process.env.PORT, () => {
   console.log("server is running ");
